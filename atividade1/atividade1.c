6afcdb4e2b49bafe6a5427ea72ad7b9c750c9b4b
Davi Gabriel Domingues <domingues.gabriel.davi@gmail.com>
2026-02-10 22:22:00 -0300

    atividade1.c

---
+++ atividade1/atividade1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n; // variável para armazenar o número de vezes que a mensagem será exibida
+
+    scanf("%d", &n); // lê o número de vezes que a mensagem deve ser exibida
+
+    printf("Hello, World! %d\n", n); // exibe a mensagem "Hello, World!" seguida do número declarado pelo usuário
+
+    return 0;
+}
