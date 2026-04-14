007e2e88f38c7295e851c4543d162a6127af3425
davigdomingues <domingues.gabriel.davi@gmail.com>
2026-04-14 14:58:53 -0300

    Reorganização de pastas 3.

--- exercicios/2026/atividade26/atividade26.c
+++ exercicios/2026/atividade26/atividade26.c
@@ -1,51 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int totalPrincipes = 0, poderMultiplicador = 0, totalGuardas = 0, forcaGuarda = 0, maiorSomaEncontrada = -1, maiorGuardaDoVencedor = -1, principeVencedor = 1, somaBase, maiorGuardaAtual;
-    if (scanf("%d", &totalPrincipes) != 1) return 0;
-
-    int somasTotais[totalPrincipes], maioresGuardaS[totalPrincipes];
-
-    for (int i = 0; i < totalPrincipes; i++) {
-        somaBase = 0;
-        maiorGuardaAtual = 0;
-
-        scanf("%d %d", &poderMultiplicador, &totalGuardas);
-
-        for (int j = 0; j < totalGuardas; j++) {
-            scanf("%d", &forcaGuarda);
-            somaBase += forcaGuarda;
-            
-            // Regra de desempate 1: Guarda mais forte (sem o poder do príncipe)
-            if (forcaGuarda > maiorGuardaAtual) {
-                maiorGuardaAtual = forcaGuarda;
-            }
-        }
-        // Poder do príncipe amplificado pelo poder dos guardas "especiais"
-        somasTotais[i] = somaBase * poderMultiplicador;
-        maioresGuardaS[i] = maiorGuardaAtual;
-    }
-
-    // Loop de comparação
-    for (int i = 0; i < totalPrincipes; i++) {
-        // Regra Principal: maior soma de forças
-        if (somasTotais[i] > maiorSomaEncontrada) {
-            maiorSomaEncontrada = somasTotais[i];
-            maiorGuardaDoVencedor = maioresGuardaS[i];
-            principeVencedor = i + 1; // 1-indexado
-        }
-
-        // Regra de Desempate 1: mesma soma, ganha o guarda individual mais forte
-        else if (somasTotais[i] == maiorSomaEncontrada) {
-            if (maioresGuardaS[i] > maiorGuardaDoVencedor) {
-                maiorGuardaDoVencedor = maioresGuardaS[i];
-                principeVencedor = i + 1;
-            }
-            // Regra de Desempate 2: Eepate em tudo, ganha o menor número (mais velho)
+    char nome1[19], nome2[19];
+    int i;
+    
+    scanf("%s", nome1);
+    scanf("%s", nome2);
+    
+    for (i = 0; nome1[i] != '\0' && nome2[i]; i++) {
+        if (nome1[i] != nome2[i]) {
+            printf("Nao sou eu");
+            return 0;
         }
     }
-
-    printf("O vencedor e o Principe %d\n", principeVencedor);
-
+    
+    // verificação se ambos os nomes se encerraram juntos (evita falso positivo)
+    if (nome1[i] == '\0' && nome2[i] == '\0') printf("Sou eu");
+    else printf("Nao sou eu");
+    
     return 0;
 }
