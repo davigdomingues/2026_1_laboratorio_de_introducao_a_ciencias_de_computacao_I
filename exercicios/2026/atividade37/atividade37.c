8502b9b9da4281c5758f458edd9d29ed88d2dcb8
Davi Gabriel Domingues <domingues.gabriel.davi@gmail.com>
2026-04-29 22:20:46 -0300

    Renumeração de exercícios.

--- exercicios/2026/atividade37/atividade37.c
+++ exercicios/2026/atividade37/atividade37.c
@@ -1,21 +1,48 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    unsigned long long a = 0, b = 1, c; // uso de long long para evitar overflow
+    int a1, a2, a3, n;
+    scanf("%d %d %d", &a1, &a2, &a3);
+
+    int *arq[3] = {&a1, &a2, &a3}, rem[3] = {0, 0, 0};
     scanf("%d", &n);
 
-    if (n == 0) printf("0\n");
-    
-    else {
-        for (int i = 2; i <= n; i++) { // loop para calcular o n-ésimo termo da sequência de Fibonacci, a partir da operação swap
-            c = a + b;
-            a = b;
-            b = c;
-        }
+    while (n--) { // loop para ler as operações
+        int op, i, j, x;
+        scanf("%d", &op);
 
-        printf("%llu\n", b); // imprime o n-ésimo termo da sequência de Fibonacci
+        if (op == 1) { // atualizacao
+            scanf("%d %d", &i, &x);
+            i--;
+            if (!rem[i]) *arq[i] = x; // atualiza o valor do arquivo se não foi removido
+        } 
+        
+        else if (op == 2) { // remoção
+            scanf("%d", &i);
+            i--;
+            if (!rem[i]) rem[i] = 1;
+        } 
+        
+        else { // op == 3, organização
+            scanf("%d %d", &i, &j);
+            i--; j--;
+
+            // operação swap, se os arquivos não forem removidos
+            if (!rem[i] && !rem[j]) {
+                int tmp = *arq[i];
+                *arq[i] = *arq[j];
+                *arq[j] = tmp;
+            }
+        }
     }
+
+    for (int k = 0; k < 3; k++) {
+        if (rem[k]) printf("NULL"); // arquivo removido
+        else printf("%d", *arq[k]); // arquivo não removido
+        if (k < 2) printf(" "); // espaço entre os arquivos
+    }
+
+    printf("\n");
 
     return 0;
 }
