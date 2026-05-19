9b32dcb90566d16aec0b2837f0e5d58c4c8d153a
davigdomingues <domingues.gabriel.davi@gmail.com>
2026-05-19 13:53:18 -0300

    Correção da solução da atividade 36.

--- exercicios/2026/atividade36/atividade36.c
+++ exercicios/2026/atividade36/atividade36.c
@@ -8,14 +8,19 @@ int main() {
     for (int i = 0; i < n; i++) scanf("%d", &moedas[i]);
 
     int esquerda = 0, direita = n - 1;
-    while (esquerda < direita) 
-        if ((moedas[esquerda] + moedas[direita]) == c) { // se a soma das moedas nas posições esquerda e direita for igual ao valor c, imprime as moedas e retorna 0
+    while (esquerda < direita) { // loop para encontrar as moedas que somam o valor c
+        int soma = moedas[esquerda] + moedas[direita]; // soma das moedas nas posições esquerda e direita
+        if (soma == c) { // se a soma das moedas nas posições esquerda e direita for igual ao valor c, imprime as moedas e retorna 0
             printf("%d %d\n", moedas[esquerda], moedas[direita]);
             return 0;
-        }
+        } 
         
-        else // se a soma das moedas nas posições esquerda e direita for diferente do valor c, decrementa a posição direita
-            --direita;
+        else if (soma < c) // se a soma das moedas nas posições esquerda e direita for menor do que o valor c, incrementa a posição esquerda
+            esquerda++;
+
+        else // se a soma das moedas nas posições esquerda e direita for maior do que o valor c, decrementa a posição direita
+            direita--;
+    }
 
     printf("Sem troco exato");
     return 0;
